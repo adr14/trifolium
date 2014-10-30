58ad261891101fb43369abb34ae29f05636b8e99
Adriana Ravagnani <adr@cs-root.dcs.aber.ac.uk>
2014-10-30 13:48:10 +0000

    second file

---
+++ weather_p1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def weatherMessage(temp):
+    if temp < 3:
+        print("wear wool trousers")
+    elif temp < 10:
+        print("wear trousers")
+    else:
+        print("wear shorts")
+
